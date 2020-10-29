e39b317741f4c2eac40a335e35bc53559577dcff
Deep Duggal <deepduggal@deeps-mbp.attlocal.net>
2020-10-28 21:36:22 -0500

    Added gsap import and refactored an animation.

--- src/js/animations.ts
+++ src/js/animations.ts
@@ -1,6 +1,8 @@
 /**
  * Animation methods
  */
+import { TweenMax, Back } from 'gsap';
 
 export const showError = (error: Element) => TweenMax.to(error, 0.5, {top: '1rem', ease: Back.easeOut.config(2), display: 'block'});
 export const hideError = (error: Element) => TweenMax.to(error, 0.3, {top: '-50%', ease: Back.easeIn.config(2), display: 'none'});
+export const scrollToCalculator = (delay: number = 0) => TweenMax.to(window, 0.3, {delay, scrollTo: window.innerHeight});
